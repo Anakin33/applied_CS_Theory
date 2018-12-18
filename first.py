b8507b4842eb9dc620154e00539b5924c4e3be3c
Scotty Arellano <Anakin33@Protonmail.com>
2018-12-18 11:49:40 -0800

    finished everything buy Free Response 8

--- first.py
+++ first.py
@@ -25,42 +25,48 @@ When you add a new nember, it creates a new bucket to store that member.
 When you look up that pre existing member it calls the has function and retrieves what is in the bucket
 
 
-1. Name one downside of using sets and dictionaries.
+2. Name one downside of using sets and dictionaries.
 Set: no index so you cant keep track of things
 Dictionary:
 Again there is no index
 
 
-1. For a list, how long does it take to look up a member by its index? How long to look it up by its value (e.g. find the number 3)?
+3. For a list, how long does it take to look up a member by its index? How long to look it up by its value (e.g. find the number 3)?
 index is instant.
 value is much longer because we have to do a for loop or binary search if we sort the list.
 
 
-1. You want to store someone's GPS coordinates (latitude and longitude). Which data structure would be most appropriate? Why?
-DataClass. Not 100% sure what the benefits are. I think its due to ease of structure. less code then a dict.
+4. You want to store someone's GPS coordinates (latitude and longitude). Which data structure would be most appropriate? Why?
+DataClass. You can compare them where you can not do that with dicts. They also require much less code. You can use .dot notation vs bracket
 
 
-1. You want to store the number of underwear, socks, shirts, and pants that a person owns. Which data structure would be most appropriate? Why?
+5. You want to store the number of underwear, socks, shirts, and pants that a person owns. Which data structure would be most appropriate? Why?
 dictionary: You want to be able to look up that info and the best way to do that is with a key value pair
 
 
-1. You want to store the items of your reading list in order of when you added it. Which data structure would be most appropriate? Why?
+6. You want to store the items of your reading list in order of when you added it. Which data structure would be most appropriate? Why?
 list. it would be easiest to append an item and then use the index for order.
 
 
-1. You want to store the ID numbers for your employees. Which data structure would be most appropriate? Why?
+7. You want to store the ID numbers for your employees. Which data structure would be most appropriate? Why?
 You would use a tuple. Since we know everyone has a name and an id we could save memeory by only worrying about those two elements.
 
 
-1. Dictionaries can be used for the same purpose as dataclasses. What are some situations and reasons where you should use a dataclass instead of a dictionary?
+8. Dictionaries can be used for the same purpose as dataclasses.
+What are some situations and reasons where you should use a dataclass instead of a dictionary?
+When you want to compare to dataclasses.
+__init__(), .__repr__(), and .__eq__() implemented for you.
+easier to add data
+its nice you declare your variable types ahead of inputtingdata
 
 
-1. What does Optional mean? When should you use it?
+9. What does Optional mean? When should you use it?
+
 '''
 '''
 ### Code challenges
 
-1. Given [1, 2, 3, 4, 5], write a list comprehension that multiplies every member of the list by 3.
+P1. Given [1, 2, 3, 4, 5], write a list comprehension that multiplies every member of the list by 3.
 
 '''
 list1 = [1,2,3,4,5]
@@ -68,7 +74,7 @@ multiplied = [item*3 for item in list1]
 #print(multiplied)
 '''
 
-1. Given [-2, 0, -1, 5, 2], write a list comprehension that filters out any number <= 0 and adds 9 to each of the remaining members.
+P2. Given [-2, 0, -1, 5, 2], write a list comprehension that filters out any number <= 0 and adds 9 to each of the remaining members.
 
 '''
 list2 = [-2, 0, -1, 5, 2]
@@ -76,14 +82,14 @@ filtered = [x+9 for x in list2 if x <= 0]
 #print(filtered)
 '''
 
-1. Given [-2, 0, -1, 5, 2], write a list comprehension that adds 9 to each member if it is > 0 or subtracts 3 if it is <= 0.
+P3. Given [-2, 0, -1, 5, 2], write a list comprehension that adds 9 to each member if it is > 0 or subtracts 3 if it is <= 0.
 '''
 l = [-2, 0, -1, 5, 2]
 filteredA = [v+9 if v > 0 else v-3 for v in l]
 #print(filteredA)
 
 '''
-1. Return [2, 1, 0, -1, -2] reversed.
+P4. Return [2, 1, 0, -1, -2] reversed.
 
 '''
 ll = [2, 1, 0, -1, -2]
@@ -91,7 +97,7 @@ ll.reverse()
 #print(ll)
 '''
 
-1. Return "Hello World" sorted alphabetically.
+P5. Return "Hello World" sorted alphabetically.
 '''
 h = 'Hello World'
 b = ''.join(sorted(h.lower()))
@@ -99,32 +105,38 @@ b = ''.join(sorted(h.lower()))
 
 '''
 
-1. Convert ["a", "b", "c"] to a set.
+P6. Convert ["a", "b", "c"] to a set.
 
 '''
 listy = ["a", "b", "c"]
 #print(set(listy))
 
 '''
-1. Given {"english": "hello", "spanish": "hola", "french": "bonjour"}, return all the greetings (without their corresponding language).
+P7. Given {"english": "hello", "spanish": "hola", "french": "bonjour"}, return all the greetings (without their corresponding language).
 
 '''
 given = {"english": "hello", "spanish": "hola", "french": "bonjour"}
 #print(given.keys())
 
 '''
-1. Given {"english": "hello", "spanish": "hola", "french": "bonjour"}, return all the languages (without their corresponding greeting).
+P8. Given {"english": "hello", "spanish": "hola", "french": "bonjour"}, return all the languages (without their corresponding greeting).
 '''
 #print(given.values())
 
 '''
 
-1. Given {"english": "hello", "spanish": "hola", "french": "bonjour"}, invert the keys and values so that the greeting is the key, and the language is the value. Use a dictionary comprehension to do this.
+P9. Given {"english": "hello", "spanish": "hola", "french": "bonjour"}, invert the keys and values so that the greeting is the key, and the language is the value. 
+Use a dictionary comprehension to do this.
+
 '''
+given2 = {"english": "hello", "spanish": "hola", "french": "bonjour"}
+dict_variable = {value:key for (key,value) in given2.items()}
+#print(dict_variable)
+
 
 '''
 
-1. Given ["a", "b", "c", "d"], return the letter "c".
+P10. Given ["a", "b", "c", "d"], return the letter "c".
 
 '''
 sen = ["a", "b", "c", "d"]
@@ -132,7 +144,7 @@ sen = ["a", "b", "c", "d"]
 
 '''
 
-1. Given ("a", "b", "c", "d"), return the letter "c".
+P11. Given ("a", "b", "c", "d"), return the letter "c".
 '''
 
 senc = ("a", "b", "c", "d")
@@ -141,16 +153,21 @@ list(senc)
 '''
 
 
-1. Given ["a", "b", "c", "d"], print each letter along with its corresponding index.
+P12. Given ["a", "b", "c", "d"], print each letter along with its corresponding index.
 
 '''
 lister = ["a", "b", "c", "d"]
 printer = [v for v in lister]
-print(printer)
+#print(printer)
 
 '''
-1. Write a function that takes as input a number x and tries to return 1/x. Consider what should happen when x == 0? What should the function's return data type be?
+P13. Write a function that takes as input a number x and tries to return 1/x. Consider what should happen when x == 0? 
+What should the function's return data type be?
 
 '''
+def divide(x):
+    if x != 0:
+        return 1/x
+print(divide(-44))
 
-
+#Return is a float.
